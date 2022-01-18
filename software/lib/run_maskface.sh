eaa9210b0fbaa591294750b62b82a71785141636
Enrico Glerean <enrico.glerean@aalto.fi>
2022-01-18 15:03:00 +0200

    added code for computing a mask from the maskface results and fix header of defaced image

--- software/lib/run_maskface.sh
+++ software/lib/run_maskface.sh
@@ -33,10 +33,24 @@ outfile="defaced"
 if [ -f $outfile".nii" ]; then
 	echo $outfile exists so I skip
 else
-	echo ">> mask_face $infile_MF -a -o $outfile"
-	mask_face $infile_MF -a -o $outfile
-	
-
+	echo ">> mask_face $infile_MF -a -e 1 -o $outfile"
+	mask_face $infile_MF -a -e 1 -o $outfile
 fi
+
+## now move the masked image to the main folder
+# first get the output to the parent folder
+echo fslmaths $outfolder/maskface/image_std_full_normfilter -mul 1 $outfolder/defaced.nii.gz -odt float
+fslmaths $outfolder/maskface/image_std_full_normfilter -mul 1 $outfolder/defaced.nii.gz -odt float
+echo fslmaths $outfolder/maskface/image_std -mul 1 $outfolder/temp.nii.gz -odt float
+fslmaths $outfolder/maskface/image_std -mul 1 $outfolder/temp.nii.gz -odt float
+# then fix the header of the defaced
+echo fslcpgeom $outfolder/temp.nii.gz $outfolder/defaced.nii.gz
+fslcpgeom $outfolder/temp.nii.gz $outfolder/defaced.nii.gz
+# now subtract to make the mask
+echo fslmaths $outfolder/temp.nii.gz -sub $outfolder/defaced.nii.gz -abs -bin $outfolder/tempmask.nii.gz
+fslmaths $outfolder/temp.nii.gz -sub $outfolder/defaced.nii.gz -abs -bin $outfolder/tempmask.nii.gz
+# now fill the holes in the mask
+echo fslmaths $outfolder/tempmask.nii.gz -fillh $outfolder/mask.nii.gz
+fslmaths $outfolder/tempmask.nii.gz -fillh $outfolder/mask.nii.gz
 
 
